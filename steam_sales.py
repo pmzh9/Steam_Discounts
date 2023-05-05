f5a4f259deb68bf684c422904094686e6c59051d
pmzh9 <rex8825@gmail.com>
2023-05-06 01:02:19 +0600

    Now it runs on headless mode and I added progress bars.

--- steam_sales.py
+++ steam_sales.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 from datetime import datetime
 import time
+from tqdm import tqdm
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")
+driver = WebDriver(options=options)
 # Current Day
 now = datetime.now()
 day_month_year = now.strftime("%d.%m.%Y")  # DDMMYYYY
@@ -13,14 +17,20 @@ day_month_year = now.strftime("%d.%m.%Y")  # DDMMYYYY
 url = "https://store.steampowered.com/search/?specials=1"
 driver.get(url)
 
-while True:
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(1)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
+total_iterations = 63
+current_iteration = 0
+
+with tqdm(total=total_iterations) as pbar:
+    while True:
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        current_iteration += 1
+        last_height = new_height
+        pbar.update(1)
 
 html = driver.page_source
 soup = BeautifulSoup(html, "html.parser")
@@ -36,7 +46,7 @@ discounted_prices = []
 links = []
 
 # appending elements to the lists
-for element in elements:
+for element in tqdm(elements):
     if element.find("span", style="color: #888888;") is not None and element.find("div", class_="col search_price discounted responsive_secondrow").contents[3] is not None:
         normal_prices.append(element.find("span", style="color: #888888;").text)
         discounted_prices.append(element.find("div", class_="col search_price discounted responsive_secondrow").contents[3])
@@ -46,14 +56,13 @@ for element in elements:
     else:
         continue
 
-# structure of excel file
+# structure of the Excel file
 my_dict = {"title": titles,
            "discount": discounts,
            "normal_price": normal_prices,
            "discounted_price": discounted_prices,
            "link": links}
-print()
-print(len(titles), len(discounts), len(normal_prices), len(discounted_prices), len(links))
+
 # creating the Excel file
 df_discounts = pd.DataFrame(my_dict)
 file_name = f"Steam-Discounts_{day_month_year}.xlsx"
